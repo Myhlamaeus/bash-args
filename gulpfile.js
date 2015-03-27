58afdd24b9de82b04f4cba6b447bacd56f68007f
Malte-Maurice Dreyer <dreyer.maltem+dev@gmail.com>
2015-03-27 20:07:16 +0000

    Switch to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp')
+var babel = require('gulp-babel')
+var sourcemaps = require('gulp-sourcemaps')
+
+gulp.task('build', function() {
+  return gulp.src('{bash-args,lib/*}.js')
+    .pipe(sourcemaps.init())
+      .pipe(babel())
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('dist'))
+})
